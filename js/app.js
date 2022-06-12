d35e1e4c196440104b9f11b094a17a96955c4fb2
Alfredo <alfredolopeznunes@gmail.com>
2022-06-11 21:02:38 -0400

    imports needed for modularization are created

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 import FetchWrapper from "./fetch-wrapper.js";
 import { eventListeners, agregarBusqueda } from "./local-storage.js";
+import { pintarPokemon } from "./events.js";
+import { errorApi } from "./error.js";
 
 //url API
 const baseApi = "https://pokeapi.co/api/v2/";
 
 //Instancia de la API
-const API = new FetchWrapper(baseApi);
+export const API = new FetchWrapper(baseApi);
 
 //metodo get de la instancia
 const getPokemon = (pokemon) => {
@@ -18,7 +20,7 @@ const getPokemon = (pokemon) => {
             obtenerPokemon(data);
         })
         .catch((error) => {
-            console.error('error1: ' + error);
+            console.error(error);
             errorApi();
         });
 };
@@ -39,40 +41,6 @@ const obtenerPokemon = (dataPokemon) => {
     agregarBusqueda();
 };
 
-export function errorApi() {
-    let msjErr = document.querySelector(".error");
-    msjErr.classList.add('show');
-    msjErr.innerHTML = `Pokemon no encontrado. <br>Revisa que el nombre este bien escrito. <br>Búsquedas por ID hasta el 898`;
-    setTimeout(() => {
-        msjErr.classList.remove('show');
-        document.querySelector("#buscarPokemon").disabled = false;
-        form.reset();
-    }, 3000);
-}
-
-const pintarPokemon = (propiedades) => {
-    const cardPokemon = document.querySelector(".pantalla-pokedex");
-
-    document.querySelector(".pantalla-pokedex").className = "pantalla-pokedex active";
-
-    cardPokemon.querySelector(".card-body-img").setAttribute("src", propiedades.imagen);
-    cardPokemon.querySelector(".card-body-title").textContent = propiedades.nombre;
-    cardPokemon.querySelector(".altua").innerHTML = "<strong>Altura: </strong>" + propiedades.altura;
-    cardPokemon.querySelector(".peso").innerHTML = "<strong>Peso: </strong>" + propiedades.peso;
-
-    const listaTipo = propiedades.tipo.map((pokeTipo) => pokeTipo.type.name).join(", ");
-    cardPokemon.querySelector(".tipo").innerHTML = "<strong>Tipo: </strong>" + listaTipo;
-
-    // pasamos el endpoint al metodo para obtener la categoria
-    API.get(propiedades.categoria).then((categoria) => {
-        const catEs = categoria.genera.findIndex((e) => e.language.name == "es");
-        cardPokemon.querySelector(".categoria").innerHTML = "<strong>Categoria: </strong>" + categoria.genera[catEs].genus;
-
-        const desEs = categoria.flavor_text_entries.findIndex((e) => e.language.name == "es");
-        cardPokemon.querySelector(".descripcion").innerHTML = "<strong>Descripión: </strong>" + categoria.flavor_text_entries[desEs].flavor_text;
-        //console.log(categoria.flavor_text_entries);
-    });
-};
 
 export const form = document.querySelector("#formulario");
 export const pokemonIngresado = document.querySelector("#buscarPokemon");
